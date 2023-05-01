b674e2e47027947eead6d6b8755cf22408fc968a
DatSpookTho <nickguin123@gmail.com>
2023-04-30 23:18:50 -0400

    Update pokedex.ts

--- data/mods/pipmons/pokedex.ts
+++ data/mods/pipmons/pokedex.ts
@@ -11,13 +11,16 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	},
 	
 	athleetah: {
-		num: 3002,
-		name: "Athleetah",
-		types: ["Normal"],
-		baseStats: {hp: 70, atk: 70, def: 70, spa: 70, spd: 70, spe: 70},
-		abilities: {0: "Oblivious"},
-		weightkg: 8,
-		eggGroups: ["Field"],
-	},
+        num: 3002,
+        name: "Athleetah",
+        types: ["Normal", "Ground"],
+        baseStats: {hp: 65, atk: 80, def: 65, spa: 65, spd: 65, spe: 145},
+        abilities: {0: "Sprinter", 1: "Limber", H: "Quick Feet"},
+        heightm: 1.6,
+        weightkg: 45,
+        color: "Yellow",
+        eggGroups: ["Field", "Human-Like"],
+        gen: 8,
+    },
 
 };
