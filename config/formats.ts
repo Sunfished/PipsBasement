959c76ae96a6a58938567972c99e0712345adb47
Mintly <nickguin123@gmail.com>
2023-05-09 19:17:39 -0400

    Revert "Update formats.ts"
    This reverts commit deb9d3a8e1d93186fcfa97e8684df52e3e4af12a.

--- config/formats.ts
+++ config/formats.ts
@@ -1324,15 +1324,18 @@ export const Formats: FormatList = [
 		},
 	},
 	{
-		name: "[Gen 9] Fakemon Kitchen",
+		name: "[Gen 8] Pipmons",
 		desc: `Test Description`,
 		threads: [
-			`None`,
+			`<a href="https://www.smogon.com/forums/threads/3695749/">Extreme Reboot</a>`,
 		],
 
 		mod: 'pipmons',
 		ruleset: ['Standard'],
-		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass'],
+		banlist: ['All Pokemon', 'All Items'],
+		unbanlist: [
+			'Pogguest','Athleetah',
+		],
 	},
 	{
 		name: "[Gen 8] Earth & Sky OU",
