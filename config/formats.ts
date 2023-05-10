24958074e8ef4484c299dc0bf417ccea767cb4ec
Kevin Le <sunfished@gmail.com>
2023-05-09 19:11:21 -0700

    Fixed Formats Data

--- config/formats.ts
+++ config/formats.ts
@@ -20,6 +20,19 @@ export const Formats: FormatList = [
 		section: "Gen 9 Mods",
 		column: 1,
 	},
+	
+	{
+		name: "[Gen 9] Fakemon Kitchen",
+		desc: `Test Description`,
+		threads: [
+			`None`,
+		],
+		mod: 'pipmons',
+		ruleset: ['Standard'],
+		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass'],
+		unbanlist: ['Pogguest','Athleetah'],
+	},
+	
 	{
 		name: "[Gen 8] Alternatium EX",
 		desc: `<b>Alternatium EX</b>: A metagame made up of only Pokemon with alternate forms exist, with all of them being seperate and unique Pokemon.`,
@@ -1322,16 +1335,6 @@ export const Formats: FormatList = [
 					 return ["Only Zacian-Crowned can have Intrepid Sword as its ability."]
 			}
 		},
-	},
-	{
-		name: "[Gen 9] Fakemon Kitchen",
-		desc: `Test Description`,
-		threads: [
-			`None`,
-		],
-		mod: 'pipmons',
-		ruleset: ['Standard'],
-		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass'],
 	},
 	{
 		name: "[Gen 8] Earth & Sky OU",
